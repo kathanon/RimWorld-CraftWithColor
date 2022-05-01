fbf733cae93cb8e5b0c72b26dcaf9e5e2f6e802b
kathanon <cower@cower.se>
2022-05-01 18:31:47 +0200

    Fixed bug where menu would not open if any pawn was missing a favourite color.

--- Source/ColorMenu.cs
+++ Source/ColorMenu.cs
@@ -14,21 +14,21 @@ namespace CraftWithColor
 
     internal class ColorMenu
     {
-        public static void Open(ITargetColor add)
+        public static void Open(ITargetColor target)
         {
             List<FloatMenuOption> menu = new List<FloatMenuOption>();
-            if (add is BillAddition)
+            if (target is BillAddition)
             {
-                menu.Add(new FloatMenuOption(Strings.Select, () => SelectColorDialog.Open(add as BillAddition)));
+                menu.Add(new FloatMenuOption(Strings.Select, () => SelectColorDialog.Open(target as BillAddition)));
                 if (State.SavedColors.Count > 0 && !MySettings.OnlyStandard)
                 {
-                    menu.Add(new FloatSubMenu(Strings.SavedColors, SubMenuItems(State.SavedColors, c => c, c => " ", add)));
+                    menu.Add(new FloatSubMenu(Strings.SavedColors, SubMenuItems(State.SavedColors, c => c, c => " ", target)));
                 }
             }
-            menu.Add(new FloatSubMenu(Strings.Favorite, FavoriteSubMenu(add)));
+            menu.Add(new FloatSubMenu(Strings.Favorite, FavoriteSubMenu(target)));
             if (!Find.IdeoManager.classicMode)
             {
-                menu.Add(new FloatSubMenu(Strings.Ideoligion, IdeoSubMenu(add)));
+                menu.Add(new FloatSubMenu(Strings.Ideoligion, IdeoSubMenu(target)));
             }
             Find.WindowStack.Add(new FloatMenu(menu)
             {
@@ -36,24 +36,27 @@ namespace CraftWithColor
             });
         }
 
-        private static List<FloatMenuOption> FavoriteSubMenu(ITargetColor add) =>
-            SubMenuItems(Find.CurrentMap.mapPawns.FreeColonists, p => p.story.favoriteColor.Value, add);
-        private static List<FloatMenuOption> IdeoSubMenu(ITargetColor add) =>
-            SubMenuItems(Find.IdeoManager.IdeosInViewOrder, i => i.ApparelColor, add);
+        private static List<FloatMenuOption> FavoriteSubMenu(ITargetColor target) =>
+            SubMenuItems(Find.CurrentMap.mapPawns.FreeColonists, p => p.story.favoriteColor, target);
+        private static List<FloatMenuOption> IdeoSubMenu(ITargetColor target) =>
+            SubMenuItems(Find.IdeoManager.IdeosInViewOrder, i => i.ApparelColor, target);
 
-        private static List<FloatMenuOption> SubMenuItems<T>(IEnumerable<T> items, Func<T, Color> getColor, ITargetColor add) =>
-            SubMenuItems(items, getColor, o => o.ToString(), add);
-        private static List<FloatMenuOption> SubMenuItems<T>(IEnumerable<T> items, Func<T, Color> getColor, Func<T, string> toString, ITargetColor add)
+        private static List<FloatMenuOption> SubMenuItems<T>(IEnumerable<T> items, Func<T, Color?> getColor, ITargetColor target) =>
+            SubMenuItems(items, getColor, o => o.ToString(), target);
+        private static List<FloatMenuOption> SubMenuItems<T>(IEnumerable<T> items, Func<T, Color?> getColor, Func<T, string> toString, ITargetColor target)
         {
             List<FloatMenuOption> menu = new List<FloatMenuOption>();
             foreach (T item in items)
             {
-                Color color = getColor(item);
-                menu.Add(new FloatMenuOption(
-                    toString(item),
-                    delegate { add.TargetColor = color; },
-                    BaseContent.WhiteTex,
-                    color));
+                Color? color = getColor(item);
+                if (color.HasValue)
+                {
+                    menu.Add(new FloatMenuOption(
+                        toString(item),
+                        delegate { target.TargetColor = color.Value; },
+                        BaseContent.WhiteTex,
+                        color.Value));
+                }
             }
             return menu;
         }
